14726f18ea5f820064c4cf89526ae4b1c58bfa31
Akash Patel <akpatel363@gmail.com>
2019-08-15 12:59:04 +0530

    Service Modified

--- src/app/CurrencyService.ts
+++ src/app/CurrencyService.ts
@@ -6,15 +6,18 @@ export class CurrencyService{
     API_URL:string = 'https://www.floatrates.com/daily/inr.json'
     constructor(private client:HttpClient){}
     getdata(){
-        var data = []
-        this.client.get(this.API_URL).subscribe(response=>{
-            console.log(response)
-            var keys = Object.getOwnPropertyNames(response)
-            keys.forEach((value,index)=>{
-                data.push(response[value])
+        return new Promise<Object[]>((resolve,reject)=>{
+            var data = []
+            this.client.get(this.API_URL).toPromise().catch(error=>{
+                console.log(error)
+            }).then(response=>{
+                console.log(response)
+                var keys = Object.getOwnPropertyNames(response)
+                keys.forEach((value,index)=>{
+                    data.push(response[value])
+                })
+                resolve(data)
             })
-            return data
-        })
-         return data
+        })    
     }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,10 @@ export class AppComponent {
   mainhold = []
   data = []
   constructor(private service:CurrencyService){
-    this.mainhold = this.service.getdata()
-    this.data  = this.mainhold;
+    this.service.getdata().then(value=>{
+      this.mainhold = value;
+      this.data= this.mainhold
+    })
     console.log(this.data)
   }
   searchStringReceieved(message:string){
